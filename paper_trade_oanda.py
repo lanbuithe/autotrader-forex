696d40079ad7895451af319f13ca5964b8f0dda5
The Lan Bui <lanbuithe@gmail.com>
2024-03-02 19:05:20 -0800

    Initialize autotrader forex

---
+++ paper_trade_oanda.py
@@ -0,0 +1,18 @@
+from autotrader import AutoTrader
+
+# Create AutoTrader instance, configure it, and run paper mode
+at = AutoTrader()
+
+at.configure(verbosity=2, 
+             feed="oanda", 
+             broker="oanda", 
+             mode="continuous", 
+             notify=1,
+             notification_provider="telegram",
+             home_currency="CAD",
+             allow_dancing_bears=True,
+             show_plot=True)
+
+at.add_strategy("ema_crossover")
+
+at.run()
