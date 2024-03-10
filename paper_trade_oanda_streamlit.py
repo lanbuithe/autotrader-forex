8cd8678514afe6ab6f37e4da929e5f0cbea900e0
The Lan Bui <lanbuithe@gmail.com>
2024-03-09 17:44:28 -0800

    Streamlit IO

---
+++ paper_trade_oanda_streamlit.py
@@ -0,0 +1,40 @@
+from autotrader import AutoTrader
+from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import datetime
+import os
+
+def trade():
+    # Create AutoTrader instance, configure it, and run paper mode
+    at = AutoTrader()
+
+    at.configure(verbosity=2, 
+                feed="oanda", 
+                broker="oanda", 
+                mode="continuous", 
+                notify=1,
+                notification_provider="telegram",
+                home_currency="CAD",
+                allow_dancing_bears=True,
+                show_plot=True)
+
+    at.add_strategy("ema_crossover")
+
+    at.run()
+
+def stop_active_bot():
+    #
+    print(f"The stop instance task has run at {datetime.now()}")
+    files = [f for f in os.listdir('./active_bots') if os.path.isfile(f)]
+    for f in files:
+        # do something
+        tokenize = f.split('_')
+        if int(tokenize[-1] > 1):
+            os.remove(f)
+            print(f"Deleted {f}")
+
+if __name__ == "__main__":
+    trade()
+    #scheduler = BlockingScheduler(timezone='US/Pacific')
+    scheduler = BlockingScheduler()
+    scheduler.add_job(stop_active_bot, 'interval', min=30)
+    scheduler.start()
