0a181a74b8136876125986613e5aa7782ed59add
The Lan Bui <lanbuithe@gmail.com>
2026-01-02 02:42:58 -0800

    Update telegram_chat_id.py

--- telegram_chat_id.py
+++ telegram_chat_id.py
@@ -1,5 +1,7 @@
 from autotrader.comms import Telegram
 
 # Instantiate the bot and run it to get chat ID
-tb = Telegram(telegram_token)
+tb = Telegram('Here is the token for bot Autotrader Forex @autotrader_forex_bot:
+
+8383809530:AAHryeDuYOkZ8FZpKEsrHjObPluXIz1NR5A')
 tb.run_bot()
