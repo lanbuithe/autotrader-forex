064eb3b3cdb2a3763616e88bc1e7ae537fc87601
The Lan Bui <lanbuithe@gmail.com>
2026-02-18 20:14:15 -0800

    Update paper_trade_oanda.py

--- paper_trade_oanda.py
+++ paper_trade_oanda.py
@@ -26,15 +26,5 @@ at.add_strategy("supertrend")
 
 at.run()
 
-###
-PORT = 8080
-
-Handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving at port {PORT}")
-    # Start the server and keep it running until you stop the script
-    httpd.serve_forever()
-
 
 
