836160a2fc069654a19b72f40898b0fdd9eee2af
The Lan Bui <lanbuithe@gmail.com>
2026-04-12 10:05:12 -0700

    Update paper_trade_oanda_streamlit.py

--- paper_trade_oanda_streamlit.py
+++ paper_trade_oanda_streamlit.py
@@ -35,8 +35,8 @@ def stop_active_bot():
 def start_scheduler():
     scheduler = BlockingScheduler(timezone='America/Vancouver')
     # interval hours, minutes, seconds
-    scheduler.add_job(trade, 'interval', hours=1)
-    scheduler.add_job(stop_active_bot, 'interval', minutes=30)
+    scheduler.add_job(trade, 'interval', minutes=15)
+    #scheduler.add_job(stop_active_bot, 'interval', minutes=30)
     scheduler.start()
 
 if __name__ == "__main__":
