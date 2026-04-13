321d7170e55cf09dcdb741ba9b9674e43a9694c7
The Lan Bui <lanbuithe@gmail.com>
2026-04-12 23:54:45 -0700

    Update paper_trade_oanda_streamlit.py

--- paper_trade_oanda_streamlit.py
+++ paper_trade_oanda_streamlit.py
@@ -19,7 +19,7 @@ def trade():
                 allow_duplicate_bars=True)
     
     at.add_strategy("ema_crossover")
-    #at.add_strategy("long_ema_crossover")
+    at.add_strategy("long_ema_crossover")
     at.add_strategy("macd")
     at.add_strategy("supertrend")
     
