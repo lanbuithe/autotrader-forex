77db4e8b930d22311d537e3f18daa88b9a9e1bd7
The Lan Bui <lanbuithe@gmail.com>
2026-04-14 20:33:07 -0700

    Update paper_trade_oanda.py

--- paper_trade_oanda.py
+++ paper_trade_oanda.py
@@ -1,24 +1,19 @@
 from autotrader import AutoTrader
 
-# Create AutoTrader instance, configure it, and run paper mode
-at = AutoTrader()
-
-at.configure(verbosity=2, 
-             feed="oanda", 
-             broker="oanda", 
-             home_currency="CAD",
-             show_plot=True)
-
-#at.configure(verbosity=2, 
-#                feed="oanda", 
-#                broker="oanda", 
-#                mode="continuous", 
-#                notify=1,
-#                notification_provider="telegram",
-#                home_currency="CAD",
-#                allow_dancing_bears=True,
-#                show_plot=True)
-
-at.add_strategy("ema_crossover")
-
-at.run()
+    at = AutoTrader()
+    at.configure(verbosity=3, 
+                feed="oanda", 
+                broker="oanda", 
+                mode="continuous", 
+                notify=1,
+                notification_provider="telegram",
+                home_currency="CAD",
+                allow_dancing_bears=True,
+                allow_duplicate_bars=True)
+    
+    at.add_strategy("ema_crossover")
+    #at.add_strategy("long_ema_crossover")
+    at.add_strategy("macd")
+    #at.add_strategy("supertrend")
+    
+    at.run()
